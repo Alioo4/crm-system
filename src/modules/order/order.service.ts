daa272321d7f697ccc87a14a9b992c940c19519e
m.alibek <a.moyliyev@agrobank.uz>
2026-04-11 13:50:52 +0500

    remove log

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -12,7 +12,6 @@ import { Role, Status } from '@prisma/client';
 import { isUUID } from 'src/common/types/isUuid';
 import {
   generateTelegramMessage,
-  sendMessageTelegram,
   sendTelegramOrderChange,
   sendTelegramOrderDone,
 } from 'src/common/utils/send-telegram.bot';
@@ -189,8 +188,6 @@ export class OrderService {
       const date = new Date(workerArrivalDate);
       if (!isNaN(date.getTime())) where.workerArrivalDate = { gte: date };
     }
-
-    await sendMessageTelegram(where)
 
     const [orders, total] = await this.prisma.$transaction([
       this.prisma.order.findMany({
