c99bcaee8b0c90c8825a5751921412b23bd109e4
m.alibek <a.moyliyev@agrobank.uz>
2026-04-16 22:08:02 +0500

    add: config

---
+++ ecosystem.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  apps: [
+    {
+      name: 'nest-api-dev',
+      script: 'dist/src/main.js',
+
+      instances: 1,
+      exec_mode: 'cluster',
+
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '300M',
+
+      kill_timeout: 5000,
+      listen_timeout: 5000,
+
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      error_file: './logs/dev/error.log',
+      out_file: './logs/dev/out.log',
+      merge_logs: true,
+
+      env: {
+        NODE_ENV: 'production',
+        PORT: 8080,
+      },
+
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 8080,
+        GIT_SHA: process.env.GIT_SHA,
+        GIT_SHORT: process.env.GIT_SHORT,
+        GIT_BRANCH: process.env.GIT_BRANCH,
+        BUILD_TIME: process.env.BUILD_TIME,
+      },
+    },
+  ],
+};
