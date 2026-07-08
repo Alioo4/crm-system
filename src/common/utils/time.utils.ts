f374446ccbf3806d8c60269066dbe2647d29770c
m.alibek <a.moyliyev@agrobank.uz>
2026-07-08 14:16:12 +0500

    feat: enhance order management by adding assignedAt fields and sorting functionality

---
+++ src/common/utils/time.utils.ts
@@ -0,0 +1,15 @@
+export function nowInTashkent(d = new Date()): string {
+  const parts = new Intl.DateTimeFormat('en-CA', {
+    timeZone: 'Asia/Tashkent',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false,
+  }).formatToParts(d);
+
+  const get = (t: string) => parts.find((p) => p.type === t)?.value ?? '';
+  return `${get('year')}-${get('month')}-${get('day')} ${get('hour')}:${get('minute')}:${get('second')}`;
+}
